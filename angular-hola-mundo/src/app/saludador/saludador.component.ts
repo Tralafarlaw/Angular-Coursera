1e10fdd0ff7b3e1b97024e93ce163e8fad0ef6c8
Miguel <tralafarlaw@gmail.com>
2020-10-06 13:16:19 -0400

    initil build wishlist and trial ended

---
+++ angular-hola-mundo/src/app/saludador/saludador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sal',
+  templateUrl: './saludador.component.html',
+  styleUrls: ['./saludador.component.css']
+})
+export class SaludadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
